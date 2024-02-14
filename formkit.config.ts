795c5c99f4dc02ba6e9689802d70d59709c16645
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2024-02-14 12:01:33 +0100

    creating createVoluntariosView from

--- formkit.config.ts
+++ formkit.config.ts
@@ -7,22 +7,26 @@ const config = {
         label: 'text-gray-500 font-semibold',
         message: 'text-red-500 font-semibold mb-5',
         wrapper: 'space-y-2 mb-3',
-        input: 'border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-cyan-500 w-full focus:ring-cyan-500',
+        input: 'border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-blue-500 w-full',
       },
       checkbox: {
-        outer: 'space-y-10',
-        options: 'grid grid-cols-2 lg:grid-cols-4',
+        outer: 'space-y-',
+        options: 'grid grid-cols-6',
+        label: 'flex',
         fieldset: 'w-full',
         legend: 'text-gray-500 font-semibold my-5',
         wrapper: '$reset flex items-center gap-2 space-y-2',
-        input: '$reset checked:bg-cyan-500 focus:outline-none '
+        input: '$reset checked:bg-blue-500 focus:outline-none m-auto',
       },
       textarea: {
         outer: 'mt-5',
         help: 'text-gray-500'
       },
+      select: {
+        legend: 'my-10',
+      },
       submit: {
-        input: '$reset bg-cyan-500 w-1/3 py-2 rounded-lg text-white font-semibold text-lg hover:bg-cyan-600 transition-colors mt-10'
+        input: '$reset bg-blue-500 w-full py-2 rounded-lg text-white font-semibold text-lg hover:bg-blue-600 transition-colors mt-10'
       }
     })
   }
--- src/views/CrearVoluntariosView.vue
+++ src/views/CrearVoluntariosView.vue
@@ -11,12 +11,68 @@ const router = useRouter()
                 @click="router.back()">Atrás</button>
         </header>
         <section class="max-w-5xl mx-auto mt-16 ">
-            <FormKit id="create-form" type="form" :action="false" :incomplete-message="false">
-                <FormKit type="text" label="Nombre" name="name" placeholder="Nombre completo" validation="required|length:3"
-                    :validation-messages="{
+            <FormKit id="create-form" type="form" submit-label="Crear voluntario" :action="`${false}`"
+                :incomplete-message="false">
+                <FormKit type="text" label="Nombre" name="name" placeholder="Ej: Daniel, Rubén..."
+                    validation="required|length:3" :validation-messages="{
                         required: 'El nombre es obligatorio',
                         length: 'El nombre es muy corto'
                     }" />
+
+                <FormKit type="text" label="Apellidos" name="apellidos" placeholder="Ej:Avomo, Nguema..."
+                    validation="required|length:3" :validation-messages="{
+                        required: 'Los apellidos son obligatorio',
+                        length: 'El apellido es muy corto'
+                    }" />
+
+                <FormKit type="tel" label="Contacto" name="contacto" placeholder="xxxxxxxxx" :validation-messages="{
+                    matches: 'El teléfono es incorrecto',
+                    required: 'El teléfono es obligatorio'
+                }" validation="matches:/^[0-9]{3}[0-9]{3}[0-9]{3}$/|required" />
+
+                <FormKit type="checkbox" label="Seleccione disponibilidad" validation="required"
+                    :validation-messages="{ required: 'Debes añadir mínimo 1 turno' }" :options="{
+                        'martesT1': 'Martes(T1)',
+                        'miercolesT1': 'Miercoles(T1)',
+                        'juevesT1': 'Jueves(T1)',
+                        'viernesT1': 'Viernes(T1)',
+                        'sabadoT1': 'Sabado(T1)',
+                        'DomingoT1': 'Doming1(T1)',
+                        'martesT2': 'Martes(T2)',
+                        'miercolesT2': 'Miercoles(T2)',
+                        'juevesT2': 'Jueves(T2)',
+                        'viernesT2': 'Viernes(T2)',
+                        'sabadoT2': 'Sabado(T2)',
+                        'DomingoT2': 'Doming1(T2)',
+                        'martesT3': 'Martes(T3)',
+                        'miercolesT3': 'Miercoles(T3)',
+                        'juevesT3': 'Jueves(T3)',
+                        'viernesT3': 'Viernes(T3)',
+                        'sabadoT3': 'Sabado(T3)',
+                        'DomingoT3': 'Doming1(T3)',
+                        'martesT4': 'Martes(T4)',
+                        'miercolesT4': 'Miercoles(T4)',
+                        'juevesT4': 'Jueves(T4)',
+                        'viernesT4': 'Viernes(T4)',
+                        'sabadoT4': 'Sabado(T4)',
+                        'DomingoT4': 'Doming1(T4)',
+
+                    }" name="disponibilidad" />
+
+                <FormKit type="select" label="Añade la congregación" name="congregacion" validation="required"
+                    :validation-messages="{ required: 'La congregación es obligatoria' }" :options="[
+                        'Timbabe',
+                        'Sampaka',
+                        'Paraiso',
+                        'Lampert',
+                        'Buena_Esperanza',
+                        'Caracolas',
+                        'Campo_Yaounde',
+                        'Inglés',
+                        'Francés',
+                        'Fang',
+                        'Ela_Nguema',
+                    ]" />
             </FormKit>
         </section>
     </main>
