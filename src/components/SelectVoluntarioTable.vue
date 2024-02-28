dace99cb69bf13fed7ac27d0f2fc8a4644218767
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2024-02-28 13:06:03 +0100

    Select table voluntario component added

---
+++ src/components/SelectVoluntarioTable.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+defineProps<{
+    title: string
+    value1: string
+    value2: string
+    value3: string
+    value4: string
+}>()
+
+
+</script>
+
+
+<template>
+    <div class="flex flex-col">
+        <select name="" id="" class="border-2 py-2 px-1 rounded-t-lg">
+            <option value="">--Selecciona un turno--</option>
+            <option :value="value1">{{ value1 }}</option>
+            <option :value="value2">{{ value2 }}</option>
+            <option :value="value3">{{ value3 }}</option>
+            <option :value="value4">{{ value4 }}</option>
+        </select>
+        <span class="bg-gray-300 font-bold text-lg px-2 py-1">{{ title }}</span>
+        <div class="h-44 border border-gray-300 shadow-xl w-full rounded-b-lg">
+
+        </div>
+    </div>
+</template>
+
--- src/views/ProgramaView.vue
+++ src/views/ProgramaView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
 
+import SelectVoluntarioTable from '@/components/SelectVoluntarioTable.vue'
 
 
 const router = useRouter()
@@ -21,84 +22,19 @@ const router = useRouter()
 
             <header class="grid grid-cols-6 my-10 gap-5">
 
-                <div class="flex flex-col">
-                    <select name="" id="" class="border-2 py-2 px-1 rounded-t-lg">
-                        <option value="">--Selecciona un turno</option>
-                        <option value="Martes T1">Martes T1</option>
-                        <option value="Martes T1">Martes T2</option>
-                        <option value="Martes T1">Martes T3</option>
-                        <option value="Martes T1">Martes T4</option>
-                    </select>
-                    <span class="bg-gray-300 font-bold text-lg px-2 py-1">Martes</span>
-                    <div class="h-44 border border-gray-300 shadow-xl w-full rounded-b-lg">
+                <SelectVoluntarioTable title="Martes" value1="martesT1" value2="martesT2" value3="martesT3"
+                    value4="martesT4" />
+                <SelectVoluntarioTable title="Miercoles" value1="miercolesT1" value2="miercolesT2" value3="miercolesT3"
+                    value4="miercolesT4" />
+                <SelectVoluntarioTable title="Jueves" value1="juevesT1" value2="juevesT2" value3="juevesT3"
+                    value4="juevesT4" />
+                <SelectVoluntarioTable title="Viernes" value1="viernesT1" value2="viernesT2" value3="viernesT3"
+                    value4="viernesT4" />
+                <SelectVoluntarioTable title="Sábado" value1="sabadoT1" value2="sabadoT2" value3="sabadoT3"
+                    value4="sabadoT4" />
+                <SelectVoluntarioTable title="Domingo" value1="domingoT1" value2="domingoT2" value3="domingoT3"
+                    value4="domingoT4" />
 
-                    </div>
-                </div>
-                <div class="flex flex-col">
-                    <select name="" id="" class="border-2 py-2 px-1 rounded-t-lg">
-                        <option value="">--Selecciona un turno</option>
-                        <option value="Martes T1">Martes T1</option>
-                        <option value="Martes T1">Martes T2</option>
-                        <option value="Martes T1">Martes T3</option>
-                        <option value="Martes T1">Martes T4</option>
-                    </select>
-                    <span class="bg-gray-300 font-bold text-lg px-2 py-1">Martes</span>
-                    <div class="h-44 border border-gray-300 shadow-xl w-full rounded-b-lg">
-
-                    </div>
-                </div>
-                <div class="flex flex-col">
-                    <select name="" id="" class="border-2 py-2 px-1 rounded-t-lg">
-                        <option value="">--Selecciona un turno</option>
-                        <option value="Martes T1">Martes T1</option>
-                        <option value="Martes T1">Martes T2</option>
-                        <option value="Martes T1">Martes T3</option>
-                        <option value="Martes T1">Martes T4</option>
-                    </select>
-                    <span class="bg-gray-300 font-bold text-lg px-2 py-1">Martes</span>
-                    <div class="h-44 border border-gray-300 shadow-xl w-full rounded-b-lg">
-
-                    </div>
-                </div>
-                <div class="flex flex-col">
-                    <select name="" id="" class="border-2 py-2 px-1 rounded-t-lg">
-                        <option value="">--Selecciona un turno</option>
-                        <option value="Martes T1">Martes T1</option>
-                        <option value="Martes T1">Martes T2</option>
-                        <option value="Martes T1">Martes T3</option>
-                        <option value="Martes T1">Martes T4</option>
-                    </select>
-                    <span class="bg-gray-300 font-bold text-lg px-2 py-1">Martes</span>
-                    <div class="h-44 border border-gray-300 shadow-xl w-full rounded-b-lg">
-
-                    </div>
-                </div>
-                <div class="flex flex-col">
-                    <select name="" id="" class="border-2 py-2 px-1 rounded-t-lg">
-                        <option value="">--Selecciona un turno</option>
-                        <option value="Martes T1">Martes T1</option>
-                        <option value="Martes T1">Martes T2</option>
-                        <option value="Martes T1">Martes T3</option>
-                        <option value="Martes T1">Martes T4</option>
-                    </select>
-                    <span class="bg-gray-300 font-bold text-lg px-2 py-1">Martes</span>
-                    <div class="h-44 border border-gray-300 shadow-xl w-full rounded-b-lg">
-
-                    </div>
-                </div>
-                <div class="flex flex-col">
-                    <select name="" id="" class="border-2 py-2 px-1 rounded-t-lg">
-                        <option value="">--Selecciona un turno</option>
-                        <option value="Martes T1">Martes T1</option>
-                        <option value="Martes T1">Martes T2</option>
-                        <option value="Martes T1">Martes T3</option>
-                        <option value="Martes T1">Martes T4</option>
-                    </select>
-                    <span class="bg-gray-300 font-bold text-lg px-2 py-1">Martes</span>
-                    <div class="h-44 border border-gray-300 shadow-xl w-full rounded-b-lg">
-
-                    </div>
-                </div>
             </header>
 
             <table class="w-full">
@@ -119,7 +55,7 @@ const router = useRouter()
                         <div class="grid grid-cols-7 col-span-4">
                             <td class="row-span-4 bg-gray-200 text-center">9:30 - 11:30</td>
                             <td class="">
-                                <div class="bg-red-100">
+                                <div class="bg-red-100 py-2 px-2">
                                     <p class="font-bold text-md">Daniel Castillo</p>
                                     <span class="text-xs text-gray-600">555552558</span>
                                 </div>
